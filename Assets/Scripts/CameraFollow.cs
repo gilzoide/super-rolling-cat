202cf8bdc9f390cdae2e0ab94c147572cedd4589
gilzoide <gilzoide@gmail.com>
2020-09-03 20:33:34 -0300

    WIP camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothTime = 0.3f;
+    
+    private Vector3 offset;
+    private Vector3 velocity = Vector3.zero;
+
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, target.position + offset, ref velocity, smoothTime);
+    }
+}
---
+++ Assets/Scripts/PrintMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class PrintMovement : MonoBehaviour
+{
+    public Text text;
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        text.text = context.ReadValue<Vector2>().ToString();
+    }
+}
