44043a12874ce33303d1190174f42b34cd8baa57
gilzoide <gilzoide@gmail.com>
2020-09-05 17:23:19 -0300

    Add LevelInfo data, add FinishLine script changing scenes, restart Player after fall

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,11 @@ public class PlayerMovement : MonoBehaviour
     public float angularSmoothTime = 0.3f;
     public Transform movementDirectionBase;
     public Rigidbody body;
+    public float restartHeight = -5f;
+
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
+    private Vector3 bodyInitialPosition;
 
     private Vector2 movementVector = Vector2.zero;
     private Vector3 velocity;
@@ -19,6 +24,13 @@ public class PlayerMovement : MonoBehaviour
         {
             body = GetComponent<Rigidbody>();
         }
+    }
+
+    void Start()
+    {
+        initialPosition = transform.position;
+        initialRotation = transform.rotation;
+        bodyInitialPosition = body.transform.position;
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -35,6 +47,24 @@ public class PlayerMovement : MonoBehaviour
         velocityForward.x += (velocityForward.x > 0.001f ? 0f : 1f) * forward.x;
         velocityForward.z += (velocityForward.z > 0.001f ? 0f : 1f) * forward.z;
         transform.rotation = Quaternion.LookRotation(Vector3.SmoothDamp(forward, velocityForward, ref velocity, angularSmoothTime));
+    }
+
+    void LateUpdate()
+    {
+        if (transform.position.y < restartHeight)
+        {
+            StartCoroutine(Restart());
+        }
+    }
+
+    IEnumerator Restart()
+    {
+        body.Sleep();
+        body.velocity = Vector3.zero;
+        body.angularVelocity = Vector3.zero;
+        body.transform.SetPositionAndRotation(bodyInitialPosition, initialRotation);
+        yield return null;
+        transform.SetPositionAndRotation(initialPosition, initialRotation);
     }
 
     void FixedUpdate()
---
+++ Assets/Scripts/ScriptableObjects/LevelInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/LevelInfo")]
+public class LevelInfo : ScriptableObject
+{
+    public int levelIndex;
+    public string nextSceneName;
+}
