4cd22e45c7351c0250a02857c33aef6324d461c8
gilzoide <gilzoide@gmail.com>
2020-09-06 19:19:51 -0300

    Add SFX when selecting UI button on MainMenu

---
+++ Assets/Scripts/PlayAudioOnSelect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayAudioOnSelect : MonoBehaviour, ISelectHandler
+{
+    public AudioSource audioSource;
+    public AudioClip clip;
+
+    void Awake()
+    {
+        if (!audioSource)
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        audioSource.PlayOneShot(clip);
+    }
+}
