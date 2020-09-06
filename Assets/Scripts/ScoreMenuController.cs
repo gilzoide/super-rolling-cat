57a502ca8628d3ce9f77ba2bba2c98a9e4633aea
gilzoide <gilzoide@gmail.com>
2020-09-06 11:34:49 -0300

    Add ScoreMenu scene

---
+++ Assets/Scripts/ScoreMenuController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ScoreMenuController : MonoBehaviour
+{
+    public Score gameplayScore;
+    public Text statsText;
+
+    void Start()
+    {
+        statsText.text = $"Points: {gameplayScore.points}\nTime: {gameplayScore.time}\nDeaths: {gameplayScore.deaths}\n";
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
