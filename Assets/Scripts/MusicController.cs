39334c8576957ed4186e7c75b667ec1d6006ed64
gilzoide <gilzoide@gmail.com>
2020-09-06 16:06:06 -0300

    Add gameplay background music and add play it continuously from MainMenu

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    public AudioSource audioSource;
+    private static MusicController playingInstance;
+
+    void Awake()
+    {
+        if (playingInstance == null)
+        {
+            playingInstance = this;
+            DontDestroyOnLoad(gameObject);
+            audioSource.Play();
+        }
+    }
+}
